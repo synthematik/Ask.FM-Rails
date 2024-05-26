9753fe8794f4e52cc8d1f6f5fa07d04ca56d6f23
Garik <garfild_2003@mail.ru>
2024-05-26 11:26:48 +0300

    Улучшена валидация: name теперь имеет максимальную длину, длина пароля уменьшена. Добавлен колбэк для перевода nickname в нижний регистр.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,18 @@
 class User < ApplicationRecord
   has_secure_password
 
+  before_save :downcase_nickname
+
   has_many :questions, dependent: :destroy
 
   mount_uploader :avatar, AvatarUploader
 
-  validates :name, presence: true
+  validates :name, presence: true, length: { maximum: 15 }
   validates :nickname, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 5 }
+
+  def downcase_nickname
+    nickname.downcase!
+  end
 end
